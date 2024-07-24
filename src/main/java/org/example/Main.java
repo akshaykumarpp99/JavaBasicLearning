9758c6351cbe97a5a76def074d9cd8291de55b4c
AkshayKumar0399 <akshay.kumar.police.patil@gds.ey.com>
2024-07-24 11:52:05 +0530

    Code Clean up

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,33 +11,33 @@ import java.util.List;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-//        List<Card> deck = Card.getStandardDeck();
-//        Card.printDeck(deck);
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
 
         Card[] cardArray = new Card[13];
         Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
-//        System.out.println(aceOfHearts);
+        System.out.println(aceOfHearts);
         Arrays.fill(cardArray, aceOfHearts);
-//        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
 
         List<Card> cards = new ArrayList<>(52);
-//        Collections.fill(cards, aceOfHearts);
-//        System.out.println(cards);
-//        System.out.println("cards.size() = " + cards.size());
+        Collections.fill(cards, aceOfHearts);
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
 
         List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
-//        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
 
         Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
         List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
-//        Card.printDeck(kingsOfClubs, "Kings of Clubs", 1);
+        Card.printDeck(kingsOfClubs, "Kings of Clubs", 1);
 
         Collections.addAll(cards, cardArray);
         Collections.addAll(cards, cardArray);
-//        Card.printDeck(cards, "Card Collection with Aces added", 2);
+        Card.printDeck(cards, "Card Collection with Aces added", 2);
 
         Collections.copy(cards, kingsOfClubs);
-//        Card.printDeck(cards, "Card Collection with Kings copied", 2);
+        Card.printDeck(cards, "Card Collection with Kings copied", 2);
 
         cards = List.copyOf(kingsOfClubs);
         Card.printDeck(cards, "List Copy of Kings", 1);
