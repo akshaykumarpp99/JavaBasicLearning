df9b59a63d82eaed76c32001cd981f1c1f0ee8a6
AkshayKumar0399 <akshay.kumar.police.patil@gds.ey.com>
2024-09-15 22:15:55 +0530

    Created the method to find the duplicate chars in the given string and called in the main function to check the functionality.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,34 @@
-import practice.CountTheNumberOfCharInString;
+import practice.FindTheDuplicateCharsInString;
 
 public class Main {
     public static void main(String[] args) {
 
-//        Sum of Digits
+////        Sum of Digits
 //        SumOfDigits sod = new SumOfDigits();
 //        sod.sumOfDigits(5142);
 
 //====================================================
 
-//        Reverse a String
+////        Reverse a String
 //        ReverseAString r1 = new ReverseAString();
 //        r1.reverseAString("Akshay");
 
-//        Swap 2 Strings
+////       Swap 2 Strings
 //        Swap2Strings s1 = new Swap2Strings();
 //        s1.swapTwoStrings("Akshay","yahskA");
 
-//        Verify whether it is prime number or not
+////        Verify whether it is prime number or not
 //        PrimeNumberOrNot p = new PrimeNumberOrNot();
 //        p.primeNumberOrNot(1);
 
-//        Count the number of characters in given String and repetations of chars in given string
-        CountTheNumberOfCharInString c = new CountTheNumberOfCharInString();
+////        Count the number of characters in given String and repetations of chars in given string
+//        CountTheNumberOfCharInString c = new CountTheNumberOfCharInString();
 //        c.countTheNumberOfCharInString("asdfghjkl");
-        c.numberOfSpecificChars("akshaya");
+//        c.numberOfSpecificChars("akshaya");
+
+//      Find the duplicate chars in the given string
+        FindTheDuplicateCharsInString f = new FindTheDuplicateCharsInString();
+        f.findTheDuplicateCharsInString("asdasdasdffffff");
 
     }
 }
