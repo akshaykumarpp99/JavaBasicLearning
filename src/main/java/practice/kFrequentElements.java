f1f2d90334a369753426ac9909d16df9a475a294
AkshayKumar0399 <akshay.kumar.police.patil@gds.ey.com>
2025-06-19 11:24:54 +0530

    Code to find the K frequent elements

---
+++ src/main/java/practice/kFrequentElements.java
@@ -0,0 +1,35 @@
+package practice;
+
+import java.util.*;
+
+public class kFrequentElements {
+    public static void main(String[] args) {
+
+//        Top K Frequent elements
+
+//        Store the key as a number and value as number of repetations
+//        create List of int array
+//        using Map.Entry interface add the value and key into the List of int array
+//        Sort the List<int[]>
+//        Create another int[] var and
+//        Get the 2nd value of List<int[]> and store in var which is created just
+//        Extract the top k elements and log it
+
+        int[] nums = {1,2,2,3,3,3};
+        int k=3;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+        List<int[]> arr = new ArrayList<>();
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            arr.add(new int[] {entry.getValue(), entry.getKey()});
+        }
+        arr.sort((a,b) -> b[0] - a[0]);
+        int[] res = new int[k];
+        for(int i=0; i < k;i++){
+            res[i] = arr.get(i)[1];
+        }
+        System.out.println(Arrays.toString(res));
+    }
+}
